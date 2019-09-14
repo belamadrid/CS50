98cd799e25ae42beff4f9cc81eab7215955f4cdb
belamadrid <belamadrid@users.noreply.github.com>
2019-09-14 19:53:32 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
+    // prints a statement to the screen
     printf("hello, world\n");
+    // gets user name input and prints it out 
     string name = get_string("What is your name?\n");
     printf("hello, %s\n", name);
 }
