b36bf10a1338834915c7b47456cf58785d66ce92
belamadrid <belamadrid@users.noreply.github.com>
2019-09-21 00:41:15 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -10,20 +10,20 @@ int main(void)
 {
     //define letter count, word count, sentence count, and index as integer
     int letters;
-    letters=0;
+    letters = 0;
     int words = 0;
     //start words at 1 because there is no space before the first word in the text
-    words=1;
+    words = 1;
     int sentences;
-    sentences=0;
+    sentences = 0;
     int i;
     int index;
 
 
-    string s= get_string("Write down your text: \n");
+    string s = get_string("Write down your text: \n");
 
     //i starts at 0 and goes up until text hits null terminator
-    for (i=0; s[i] != '\0'; i++)
+    for (i = 0; s[i] != '\0'; i++)
     {
         //ctype.h detects letters in string
         if (isalpha(s[i]))
@@ -32,7 +32,7 @@ int main(void)
         }
     }
 
-    for (i=0; s[i] != '\0'; i++)
+    for (i = 0; s[i] != '\0'; i++)
     {
         //ctype.h detects spaces in string which can represent number of words if you start counting at 1
         if (isspace(s[i]))
@@ -41,36 +41,36 @@ int main(void)
         }
     }
 
-    for (i=0; s[i] != '\0'; i++)
+    for (i = 0; s[i] != '\0'; i++)
     {
         //detects sentence as number of . or ? or !
-        if (s[i]=='.' || s[i]=='?' || s[i]=='!')
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
         {
             sentences++;
         }
     }
 
-// Theo Lauriette showed me that I had to express L, S, letters, and words as floats because they were used to calculate the average
-float L = 100 * (float) letters / (float) words;
-float S = 100 * (float) sentences / (float) words;
+    // Theo Lauriette showed me that I had to express L, S, letters, and words as floats because they were used to calculate the average which should use real numbers and not just integers
+    float L = 100 * (float) letters / (float) words;
+    float S = 100 * (float) sentences / (float) words;
 
 
-index = round(0.0588 * L - 0.296 * S - 15.8);
+    index = round(0.0588 * L - 0.296 * S - 15.8);
 
-if (index>=16)
-{
-    printf("Grade 16+\n");
-}
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
 
-else if (index<1)
+    else if (index < 1)
 
-{
-    printf("Before Grade 1\n");
-}
-else
-{
-    printf("Grade %i\n", index);
-}
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 
 
 }
