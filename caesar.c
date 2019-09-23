83cdfbf48c47a5d8961c152deb70fbbba3ffe494
belamadrid <belamadrid@users.noreply.github.com>
2019-09-23 01:00:40 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -11,16 +11,16 @@ int main(int argc, string argv[])
     int i;
     int n;
 
-//checks if argument count is not equal to 2
+    //checks if argument count is not equal to 2
     if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
-//https://stackoverflow.com/questions/29248585/c-checking-command-line-argument-is-integer-or-not
-//so that goes through each i in the string in order to tell if it is a nondigit
-    for (i = 0; i<strlen(argv[1]); i++)
+    //https://stackoverflow.com/questions/29248585/c-checking-command-line-argument-is-integer-or-not
+    //so that goes through each part of the string in order to tell if it is a nondigit
+    for (i = 0; i < strlen(argv[1]); i++)
     {
         if (!isdigit(argv[1][i]))
         {
@@ -30,19 +30,19 @@ int main(int argc, string argv[])
     }
 
 
- //define key as integer
+    //define key as integer
     int k = atoi(argv[1]);
 
 
-    string s= get_string("plaintext:  ");
+    string s = get_string("plaintext: ");
 
     printf("ciphertext: ");
 
-//goes through each part of string
-    for (i = 0; i<strlen(s); i++)
+    //goes through each part of string
+    for (i = 0; i < strlen(s); i++)
     {
         //prints non alphabetical characters as plaintext
-        if(!isalpha(s[i]))
+        if (!isalpha(s[i]))
         {
             printf("%c", s[i]);
         }
@@ -51,14 +51,14 @@ int main(int argc, string argv[])
         //prints upper case alphabetical characters through ciphertext
         if (isalpha(s[i]) && isupper(s[i]))
         {
-            printf("%c", (((s[i] - 65)+k)%26)+65);
+            printf("%c", (((s[i] - 65) + k) % 26) + 65);
 
         }
 
         //prints lower case alphabetical characters through ciphertext
         if (isalpha(s[i]) && islower(s[i]))
         {
-            printf("%c", (((s[i] - 97)+k)%26)+97);
+            printf("%c", (((s[i] - 97) + k) % 26) + 97);
 
         }
     }
