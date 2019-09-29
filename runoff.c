04b63c39f88ad8ba12d1cf2abf621add1ef3afc2
belamadrid <belamadrid@users.noreply.github.com>
2019-09-29 21:17:16 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -170,6 +170,7 @@ bool print_winner(void)
         if (candidates[i].votes > (voter_count/2) )
             {
                 printf("%s\n", candidates[i].name);
+                return true;
             }
     }
     return false;
