96afea05261dce108823dc4b872554d972fb942c
belamadrid <belamadrid@users.noreply.github.com>
2019-10-05 20:51:32 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -28,12 +28,10 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
    {
        for(int j=0; j<width; j++)
        {
-           int sepiaRed = round( (float) .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-           int sepiaGreen = round( (float) .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
-           int sepiaBlue = round ((float) .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
-           sepiaRed= round (sepiaRed);
-           sepiaGreen= round (sepiaGreen);
-           sepiaBlue= round(sepiaBlue);
+           int sepiaRed = round( .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+           int sepiaGreen = round( .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+           int sepiaBlue = round ( .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
            if (sepiaRed >255)
            {
                sepiaRed=255;
