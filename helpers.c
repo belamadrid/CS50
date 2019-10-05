3b5c4f8a8c053f24495586425c1960b7f8578949
belamadrid <belamadrid@users.noreply.github.com>
2019-10-05 20:13:42 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,14 +1,21 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+
    for (int i=0; i<= height; i++)
    {
        for(int j=0; i<=width; i++)
        {
-           RGBTRIPLE pixel = RGBTRIPLE image[i][j]
-           round (float average = ((pixel.rgbtRED + pixel.rgbtBLUE pixel.rgbtGREEN)/3));
+           RGBTRIPLE pixel = image[i][j];
+           float average = ((pixel.rgbtRed + pixel.rgbtBlue + pixel.rgbtGreen)/3);
+           average = round (average);
+           pixel.rgbtRed = average;
+           pixel.rgbtBlue = average;
+           pixel.rgbtGreen = average;
 
        }
    }
@@ -18,6 +25,37 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for (int i=0; i<= height; i++)
+   {
+       for(int j=0; i<=width; i++)
+       {
+           RGBTRIPLE pixel = image[i][j];
+           float sepiaRed = .393 * pixel.rgbtRed + .769 * pixel.rgbtGreen + .189 * pixel.rgbtBlue;
+           float sepiaGreen = .349 * pixel.rgbtRed + .686 * pixel.rgbtGreen + .168 * pixel.rgbtBlue;
+           float sepiaBlue = .272 * pixel.rgbtRed + .534 * pixel.rgbtGreen + .131 * pixel.rgbtBlue;
+           sepiaRed= round (sepiaRed);
+           sepiaGreen= round (sepiaGreen);
+           sepiaBlue= round(sepiaBlue);
+           if (sepiaRed >255)
+           {
+               sepiaRed=255;
+           }
+
+           if (sepiaGreen >255)
+           {
+               sepiaGreen=255;
+           }
+
+           if (sepiaBlue >255)
+           {
+               sepiaBlue=255;
+           }
+
+
+       }
+   }
+
     return;
 }
 
