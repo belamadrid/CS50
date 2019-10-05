85c24710989e76450893b33e02c36a890b15acec
belamadrid <belamadrid@users.noreply.github.com>
2019-10-05 21:31:39 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -59,11 +59,30 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+   for (int i=0; i< height; i++)
+   {
+       for(int j=0; j<width; j++)
+       {
+           image[i][j]=image[i][j];
+       }
+   }
+
+
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i=0; i< height; i++)
+   {
+       for(int j=0; j<width; j++)
+       {
+           int redaverage =round( (float)(image[i-1][j-1].rgbtRed + image[i-1][j].rgbtRed + image[i-1][j+1].rgbtRed + image[i][j-1].rgbtRed + image[i][j].rgbtRed + image[i][j+1].rgbtRed + image[i+1][j-1].rgbtRed + image[i+1][j].rgbtRed + image[i+1][j+1].rgbtRed));
+           int blueaverage =round( (float)(image[i-1][j-1].rgbtBlue + image[i-1][j].rgbtBlue + image[i-1][j+1].rgbtBlue + image[i][j-1].rgbtBlue + image[i][j].rgbtBlue + image[i][j+1].rgbtBlue + image[i+1][j-1].rgbtBlue + image[i+1][j].rgbtBlue + image[i+1][j+1].rgbtBlue));
+           int greenaverage =round( (float)(image[i-1][j-1].rgbtGreen + image[i-1][j].rgbtGreen + image[i-1][j+1].rgbtGreen + image[i][j-1].rgbtGreen + image[i][j].rgbtGreen + image[i][j+1].rgbtGreen + image[i+1][j-1].rgbtGreen + image[i+1][j].rgbtGreen + image[i+1][j+1].rgbtGreen));
+       }
+   }
     return;
 }
