13641ff2526ed769a4b9ceeb09c35bdb1167483b
belamadrid <belamadrid@users.noreply.github.com>
2019-10-06 15:39:44 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -63,7 +63,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
    {
        for(int j=0; j<width/2; j++)
        {
+           float a= image[i][j];
            image[i][j]=image[i][width - 1 - j];
+           image[i][width - 1 - j] = a;
        }
    }
 
