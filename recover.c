56385af4cda496be6c0fe40718d8db4c83c10863
belamadrid <belamadrid@users.noreply.github.com>
2019-10-06 20:03:37 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     FILE* outfile = fopen(filename ,"w");
     while (fread( buff, 1, 512, file) == 512)
     {
-        if (buff[0]== 0xff && buff[1] == 0xd8 && buff[2] == 0xff)
+        if (buff[0]== 0xff && buff[1] == 0xd8 && buff[2] == 0xff && (buff[3] & 0xf0) == 0xe0)
         {
             if(jpgnum > 0)
             {
